2e87cd5cb9f5b41fbfd9aba08383e60e387fe5a5
Fazliddin <fazliddinidiev@gmail.com>
2023-11-14 15:28:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let a = 13;
+for (let i = 0; i < a; i++)
+{
+    console.log(i);
+}
+console.log(a); 
+let b = 13;
+let c = 'string';
+let d = Boolean;
+console.log(b);
+console.log(c);
+console.log(d);
+let v = 13;
+console.log(v);
+
+
